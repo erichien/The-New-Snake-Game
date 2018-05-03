4b93c0a8faacff29e10fa0044189c03f936438f7
Eric <17807730+virtuositeit@users.noreply.github.com>
2018-05-03 13:40:06 -0400

    update game logic, snake moving forward

--- public/js/script.js
+++ public/js/script.js
@@ -1,39 +1,3 @@
-let _stringToNums = foodPosString => {
-  return foodPosString.split(',').map(x => parseInt(x));
-};
-
-let _numsToString = pos => {
-  return `${pos[0]},${pos[1]}`;
-};
-
-let _assignCellColor = (cell, i, j) => {
-  if ((i + j) % 2 == 0) {
-    cell.style.backgroundColor = '#B7D65C';
-    // cell.style.backgroundColor = '#B6D856';
-  } else {
-    cell.style.backgroundColor = '#AFCF55';
-    // cell.style.backgroundColor = '#A1CB4A';
-  }
-};
-
-let _renderSnake = (grid, snakeBody) => {
-  for (let pos of snakeBody) {
-    let cell = grid.querySelector(
-      `div[data-cell-pos=${'"' + _numsToString(pos) + '"'}]`
-    );
-    cell.querySelector('.button').style.display = 'flex';
-  }
-};
-
-let _eraseSnake = (grid, snakeBody) => {
-  for (let pos of snakeBody) {
-    let cell = grid.querySelector(
-      `div[data-cell-pos=${'"' + _numsToString(pos) + '"'}]`
-    );
-    cell.querySelector('.button').style.display = 'none';
-  }
-};
-
 (() => {
   document.addEventListener('DOMContentLoaded', () => {
     let socket = io(); // websocket to server
@@ -41,10 +5,17 @@ let _eraseSnake = (grid, snakeBody) => {
 
     // game states
     let gameInPlay = false;
-    let snakeBody = null;
-    let snakeBodySet = new Set();
-    let score = 0;
+    let snakeBody = [[3, 3], [3, 4]];
     let foodPos = null;
+    let score = 0;
+
+    let _stringToNums = foodPosString => {
+      return foodPosString.split(',').map(x => parseInt(x));
+    };
+
+    let _numsToString = pos => {
+      return `${pos[0]},${pos[1]}`;
+    };
 
     let _mouseenterHandler = event => {
       event.currentTarget.querySelector('.button').style.display = 'flex';
@@ -58,40 +29,47 @@ let _eraseSnake = (grid, snakeBody) => {
       _placeFood(event);
     };
 
-    // how to pass socket and foodPos here?
-    let _placeFood = event => {
-      if (!foodPos) {
-        foodPos = _stringToNums(event.currentTarget.dataset.cellPos);
-
-        event.currentTarget.removeEventListener(
-          'mouseenter',
-          _mouseenterHandler
-        );
-        event.currentTarget.removeEventListener(
-          'mouseleave',
-          _mouseleaveHandler
-        );
-        event.currentTarget.removeEventListener('click', _clickHandler);
-      }
-
-      event.currentTarget.querySelector('.button').style.display = 'flex';
-      socket.emit('foodPlaced', foodPos);
-      console.log('food placed at', foodPos);
+    let _addEventListeners = element => {
+      element.addEventListener('mouseenter', _mouseenterHandler);
+      element.addEventListener('mouseleave', _mouseleaveHandler);
+      element.addEventListener('click', _clickHandler);
     };
 
-    // _removeFood when food is eaten (state.foodPos = null)
+    let _removeEventListeners = element => {
+      element.removeEventListener('mouseenter', _mouseenterHandler);
+      element.removeEventListener('mouseleave', _mouseleaveHandler);
+      element.removeEventListener('click', _clickHandler);
+    };
+
+    let _placeFood = event => {
+      if (!foodPos) {
+        let cell = event.currentTarget;
+        foodPos = _stringToNums(cell.dataset.cellPos);
+        _removeEventListeners(cell);
+        cell.querySelector('.button').style.display = 'flex';
+        socket.emit('foodPlaced', foodPos);
+        console.log('food placed at', foodPos);
+      }
+    };
+
+    // remove food when food is eaten (state.foodPos = null)
     let _removeFood = stateFoodPos => {
-      // take care of markup at foodPos
       let cell = grid.querySelector(
         `div[data-cell-pos=${'"' + _numsToString(foodPos) + '"'}]`
       );
-      cell.addEventListener('mouseenter', _mouseenterHandler);
-      cell.addEventListener('mouseleave', _mouseleaveHandler);
-      cell.addEventListener('click', _clickHandler);
 
+      _addEventListeners(cell);
       cell.querySelector('.button').style.display = 'none';
       foodPos = null;
       console.log('food removed');
+    };
+
+    let _assignCellColor = (cell, i, j) => {
+      if ((i + j) % 2 == 0) {
+        cell.style.backgroundColor = '#B7D65C';
+      } else {
+        cell.style.backgroundColor = '#AFCF55';
+      }
     };
 
     let _createBoard = (grid, socket, foodPos) => {
@@ -105,14 +83,33 @@ let _eraseSnake = (grid, snakeBody) => {
           cell.setAttribute('class', 'cell');
           cell.setAttribute('data-cell-pos', `${i},${j}`);
           _assignCellColor(cell, i, j);
-
-          cell.addEventListener('mouseenter', _mouseenterHandler);
-          cell.addEventListener('mouseleave', _mouseleaveHandler);
-          cell.addEventListener('click', _clickHandler);
+          _addEventListeners(cell);
 
           cell.appendChild(button);
           grid.appendChild(cell);
         }
+      }
+    };
+
+    let _renderSnake = (grid, snakeBody) => {
+      for (let pos of snakeBody) {
+        let cell = grid.querySelector(
+          `div[data-cell-pos=${'"' + _numsToString(pos) + '"'}]`
+        );
+
+        cell.style.backgroundColor = '#eeb82b';
+        _removeEventListeners(cell);
+      }
+    };
+
+    let _eraseSnake = (grid, snakeBody) => {
+      for (let pos of snakeBody) {
+        let cell = grid.querySelector(
+          `div[data-cell-pos=${'"' + _numsToString(pos) + '"'}]`
+        );
+
+        _assignCellColor(cell, pos[0], pos[1]);
+        _addEventListeners(cell);
       }
     };
 
@@ -129,7 +126,6 @@ let _eraseSnake = (grid, snakeBody) => {
       _eraseSnake(grid, snakeBody);
       gameInPlay = state.gameInPlay;
       snakeBody = state.snakeBody;
-      snakeBodySet = state.snakeBodySet;
       score = state.score;
       _renderSnake(grid, snakeBody);
 
--- server.js
+++ server.js
@@ -2,8 +2,8 @@ let express = require('express'); // web server application
 let app = express(); // webapp
 let http = require('http').Server(app); // connects http library to server
 let io = require('socket.io')(http); // connect websocket library to server
-let serialPort = require('serialport'); // serial library
-let readLine = serialPort.parsers.Readline; // read serial data as lines
+// let serialPort = require('serialport'); // serial library
+// let readLine = serialPort.parsers.Readline; // read serial data as lines
 let serverPort = 8000;
 
 //---------------------- WEBAPP SERVER SETUP ---------------------------------//
@@ -22,32 +22,103 @@ app.get('/', function(req, res) {
 //----------------------------------------------------------------------------//
 
 //------------------------------- GAME STATES --------------------------------//
+
+// game environment variables
+let boardHeight = 8;
+let boardWidth = 8;
+
+let dirs = {
+  U: [-1, 0],
+  D: [1, 0],
+  L: [0, -1],
+  R: [0, 1]
+};
+
+// connections
 let clientReady = false;
 let arduinoReady = false;
+
+// game state variables
 let gameInPlay = false;
 let score = 0;
 let foodPos = [0, 0]; // when food is eaten => null
-let dir = 'right'; // random?
-let snakeBody = [[3, 3], [3, 4], [3, 5], [3, 6]];
+let dir = 'R'; // 'U', 'D', 'L', 'R', randomize on start?
+let snakeBody = [[3, 0], [3, 1], [3, 2]];
 let snakeBodySet = new Set();
-let gameStatus = {};
 
-let _updateGameStatus = () => {
-  gameStatus = {
+let _updateClient = () => {
+  clientGameStatus = {
     gameInPlay: gameInPlay,
-    score: score,
-    foodPos: foodPos,
-    dir: dir,
     snakeBody: snakeBody,
-    snakeBodySet: snakeBodySet
+    foodPos: foodPos,
+    score: score
   };
+  io.emit('gameUpdate', clientGameStatus);
+};
+
+let _numsToString = pos => {
+  return `${pos[0]},${pos[1]}`;
+};
+
+let _stringToNums = foodPosString => {
+  return foodPosString.split(',').map(x => parseInt(x));
+};
+
+let _calculateNextState = () => {
+  let curHead;
+  let newHead;
+  let curI;
+  let curJ;
+  let dI;
+  let dJ;
+  let i;
+  let j;
+
+  curHead = snakeBody[snakeBody.length - 1];
+  [curI, curJ] = curHead;
+  [dI, dJ] = dirs[dir];
+  newHead = [curI + dI, curJ + dJ];
+  [i, j] = newHead;
+
+  // check if game over
+  if (
+    i < 0 ||
+    i == boardHeight ||
+    j < 0 ||
+    j == boardWidth ||
+    (snakeBodySet.has(_numsToString(newHead)) && newHead != snakeBody[0])
+  ) {
+    // game over
+    gameInPlay = false;
+    console.log('game over');
+    return -1; //?
+  }
+
+  if (foodPos && [i, j] == foodPos[0]) {
+    score += 1;
+    foodPos = null;
+  } else {
+    let tail = snakeBody.shift();
+    snakeBodySet.delete(_numsToString(tail));
+  }
+
+  snakeBody.push(newHead);
+  snakeBodySet.add(_numsToString(newHead));
+};
+
+let _gameLoop = () => {
+  _calculateNextState();
+  console.log(snakeBody);
+  _updateClient(); // update client
+  // _updateArduino(); // update arduino
+  console.log('Cant stop me now!');
 };
 
 //----------------------------------------------------------------------------//
 
 //---------------------- SERIAL COMMUNICATION --------------------------------//
 // start the serial port connection and read on newlines
-
+/*
 const serial = new serialPort('/dev/ttyUSB0', {
   baudRate: 115200
 });
@@ -58,12 +129,12 @@ const parser = new readLine({
 // TODO: calculate whether or not to change snake's moving direction: e.g. can't move opposite dir
 
 // Read data that is available on the serial port and send it to the websocket
+
 serial.pipe(parser);
 parser.on('data', data => {
   // on data from the arduino
   if (data == 'up') {
     dir = 'up';
-    io.emit('');
   } else if (data == 'down') {
     dir = 'down';
   } else if (data == 'left') {
@@ -77,6 +148,10 @@ parser.on('data', data => {
       if (clientReady) {
         gameInPlay = true;
         console.log('gameInPlay:', true);
+
+        // start game, 500 millisecs/frame
+        setInterval(_gameLoop, 500);
+
         //send initial matrix to arduino and client
         var stringTosend = 'matrix:' + snakeBody.toString();
         serial.write(stringTosend, function(err) {
@@ -90,9 +165,23 @@ parser.on('data', data => {
   }
 });
 
-//----------------------------------------------------------------------------//
+let _updateArduino() => {
 
-// io.emit('gameUpdate', a);
+  // send game update to arduino
+  var stringTosend =
+    'matrix:' + snakeBody.toString() + ';foodPosition:' + foodPos.toString();
+  console.log(stringTosend);
+  serial.write(stringTosend, function(err) {
+    if (err) {
+      return console.log('Error on write: ', err.message);
+    }
+    console.log('message written');
+  });
+}
+
+*/
+
+//----------------------------------------------------------------------------//
 
 //---------------------- WEBSOCKET COMMUNICATION -----------------------------//
 
@@ -103,9 +192,13 @@ io.on('connect', function(socket) {
   clientReady = true;
   console.log('connected to client');
 
-  if (arduinoReady) {
+  if (clientReady) {
+    ////////////// change back to arduinoReady
     gameInPlay = true;
     console.log('gameInPlay:', true);
+
+    // start game, 500 millisecs/frame
+    setInterval(_gameLoop, 1000);
   }
 
   // can only listen for foodPlaced event via socket here
@@ -118,22 +211,11 @@ io.on('connect', function(socket) {
     foodPos = pos;
     console.log(foodPos);
     // foodPos = null;
-    _updateGameStatus();
+    _updateClient();
     io.emit('gameUpdate', gameStatus);
     console.log('food received:', foodPos);
-    // take care of arduino
-    var stringTosend =
-      'matrix:' + snakeBody.toString() + ';foodPosition:' + foodPos.toString();
-    console.log(stringTosend);
-    serial.write(stringTosend, function(err) {
-      if (err) {
-        return console.log('Error on write: ', err.message);
-      }
-      console.log('message written');
-    });
   });
-  _updateGameStatus();
-  io.emit('gameUpdate', gameStatus);
+  // take care of arduino
 
   // if you get the 'disconnect' message, say the user disconnected
   io.on('disconnect', function() {
@@ -141,3 +223,57 @@ io.on('connect', function(socket) {
     console.log('disconnected from client');
   });
 });
+
+/*
+class SnakeGame(object):
+
+    def __init__(self, width, height, food):
+        """
+        Initialize your data structure here.
+        @param width - screen width
+        @param height - screen height
+        @param food - A list of food positions
+        E.g food = [[1,1], [1,0]] means the first food is positioned at [1,1], the second is at [1,0].
+        :type width: int
+        :type height: int
+        :type food: List[List[int]]
+        """
+        self.snake_pos = set([(0, 0)])
+
+    def move(self, direction):
+        """
+        Moves the snake.
+        @param direction - 'U' = Up, 'L' = Left, 'R' = Right, 'D' = Down
+        @return The game's score after the move. Return -1 if game over.
+        Game over when snake crosses the screen boundary or bites its body.
+        :type direction: str
+        :rtype: int
+        """
+
+        # be careful!! the next pos if head could be where tail is now
+
+        print direction
+        cur_i, cur_j = cur_head = self.snake[-1]
+        d_i, d_j = self.dirs[direction]
+        new_head = cur_i + d_i, cur_j + d_j
+        i, j = new_head
+
+        if i < 0 or i == self.height or j < 0 or j == self.width or (new_head in self.snake_pos and new_head != self.snake[0]):
+            return -1
+
+        if self.food and [i, j] == self.food[0]:
+            self.food.popleft()
+        else:
+            tail = self.snake.popleft()
+            self.snake_pos.remove(tail)
+
+        self.snake.append(new_head)
+        self.snake_pos.add(new_head)
+
+        return len(self.snake) - 1
+
+
+# Your SnakeGame object will be instantiated and called as such:
+# obj = SnakeGame(width, height, food)
+# param_1 = obj.move(direction)
+*/
